d1730cef3a7dd32a86efd87a66339525a238641e
Øyvind Isaksen <oyvind.isaksen@acando.no>
2016-12-22 20:41:46 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+//importer nødvendige bibliotek
+var Express = require('express');
+var Firebase = require('firebase');
+
+//opprett en instans av Express
+var app = Express();
+
+//start serveren med å lytte på port 5555, logg en beskjed til konsollet.
+//merk at vi setter serveren til  å lytte på den porten den får tildelt av kjøremiljøet,
+//eller 5555 om den eksempelvis kjører på localhost
+var server = app.listen(process.env.PORT || 5555, function () {
+  console.log('API lytter på port: ' + server.address().port);
+});
+
+//Enkel rute på rot som sender en bekreftende beskjed på responsen
+app.get('/', function (req, res) {
+  res.send('API svarer!')
+});
