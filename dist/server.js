4af76e3ac81b4703003b7305c70e7dbddbc1c4b9
Øyvind Isaksen <oyvind.isaksen@acando.no>
2016-12-22 20:48:08 +0100

    add dist

---
+++ dist/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+
+app.use(express.static(__dirname));
+
+//start serveren med å lytte på port 5555, logg en beskjed til konsollet.
+//merk at vi setter serveren til  å lytte på den porten den får tildelt av kjøremiljøet,
+//eller 5555 om den eksempelvis kjører på localhost
+var server = app.listen(process.env.PORT || 5555, function () {
+  console.log('API lytter på port: ' + server.address().port);
+});
+
+//Enkel rute på rot som sender en bekreftende beskjed på responsen
+app.get('/', function (req, res) {
+  res.send('API svarer!')
+});
