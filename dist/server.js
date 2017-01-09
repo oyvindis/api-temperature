41768a11f86967dc334a4768f11bd76bd55b7142
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-01-09 13:15:24 +0100

    country:no

--- dist/server.js
+++ dist/server.js
@@ -67,7 +67,7 @@ app.get('/places/autocomplete/:query', function (req, res) {
 
   console.log("test");
 
-  locations.autocomplete({input: req.params.query, types: "(cities)", language: "nb", country: 'no'}, function(err, response) {
+  locations.autocomplete({input: req.params.query, types: "(cities)", language: "nb", country: 'no', components: 'country:no'}, function(err, response) {
     console.log("autocomplete: ", response.predictions);
     //console.log("autocomplete2: ", response.predictions[0].description);
     //console.log("did you mean: ", response.result);
